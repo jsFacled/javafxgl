6b0d5edb894158a51d7932e5187c4cf00cbf290f
faGitLab <facled@yahoo.com.ar>
2024-01-17 17:32:18 -0300

    agregando entidad image

--- fxgl-demoImage/src/main/java/org/example/App.java
+++ fxgl-demoImage/src/main/java/org/example/App.java
@@ -1,13 +1,34 @@
 package org.example;
 
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.entity.Entity;
+
+import static com.almasb.fxgl.dsl.FXGL.getGameWorld;
+import static com.almasb.fxgl.dsl.FXGLForKtKt.spawn;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App extends GameApplication {
+
+    private final AppFactory appFactory = new AppFactory();
+    private Entity image;
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+
+        gameSettings.setTitle("DemoImage");
     }
+
+    @Override
+    protected void initGame() {
+        getGameWorld().addEntityFactory(appFactory);
+
+        spawn("image",100,0);
+
+    }
+
+    //fin
+    public static void main( String[] args ) { launch(args); }
 }
---
+++ fxgl-demoImage/src/main/java/org/example/AppFactory.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.EntityFactory;
+import com.almasb.fxgl.entity.SpawnData;
+import com.almasb.fxgl.entity.Spawns;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import static com.almasb.fxgl.dsl.FXGLForKtKt.entityBuilder;
+import static org.example.AppType.IMAGE;
+
+public class AppFactory implements EntityFactory {
+
+@Spawns("image")
+public Entity newImage(SpawnData data){
+
+    return entityBuilder()
+            .type(IMAGE)
+            .view(new Rectangle(50,50, Color.CADETBLUE))
+            .build();
+}
+}
