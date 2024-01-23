4dd2cf4dbe166ea3f590f168b80e97f18c642584
faGitLab <facled@yahoo.com.ar>
2024-01-23 08:17:58 -0300

    Se agrega al gameWorld un player aleatorio y background

--- demoImageAnim/src/main/java/App.java
+++ demoImageAnim/src/main/java/App.java
@@ -1,12 +1,19 @@
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.entity.Entity;
 import javafx.scene.control.Dialog;
+import javafx.scene.input.KeyCode;
 
+import static com.almasb.fxgl.dsl.FXGL.*;
 /**
  * Hello world!
  *
  */
 public class App extends GameApplication{
+    private final AppFactory appFactory = new AppFactory();
+    private Entity player;
+    private Entity backround;
+
 
     @Override
     protected void initSettings(GameSettings settings) {
@@ -16,7 +23,37 @@ public class App extends GameApplication{
         settings.setHeight(600);
     }
 
-//fin----------------------------------------------
+    @Override
+    protected void initInput() {
+        onKey(KeyCode.A, () -> {
+            player.translateX(-5.0);
+        });
+        onKey(KeyCode.D, () -> {
+            player.translateX(5.0);
+        });
+        onKey(KeyCode.W, () -> {
+            player.translateY(-5.0);
+        });
+        onKey(KeyCode.S, () -> {
+            player.translateY(5.0);
+        });
+    }
+
+    @Override
+    protected void initGame() {
+        getGameWorld().addEntityFactory(appFactory);
+
+        backround = spawn("background");
+
+
+        player = spawn("player");
+        set("player", player);
+
+    }
+
+
+
+    //fin----------------------------------------------
     public static void main( String[] args ){
         launch(args);
     }
---
+++ demoImageAnim/src/main/java/AppFactory.java
@@ -0,0 +1,42 @@
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.EntityFactory;
+import com.almasb.fxgl.entity.SpawnData;
+import com.almasb.fxgl.entity.Spawns;
+import com.almasb.fxgl.entity.components.IrremovableComponent;
+import com.almasb.fxgl.minigames.randomoccurrence.RandomOccurrenceView;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
+import static com.almasb.fxgl.dsl.FXGLForKtKt.entityBuilder;
+
+public class AppFactory implements EntityFactory {
+
+    private String imageBackground = "bosque.jpg";
+    private String imagePlayer = "";
+
+    @Spawns("player")
+    public Entity newPlayer(SpawnData data) {
+
+        return entityBuilder()
+                .type(AppType.PLAYER)
+                .viewWithBBox(new RandomOccurrenceView())
+                //.with(new PlayerComponent())
+                .at(getAppWidth()/2,getAppHeight()/2)
+                .collidable()
+                 .build();
+
+    }
+
+    @Spawns("background")
+    public Entity newBackground(SpawnData data) {
+        return entityBuilder()
+                .type(AppType.BACKGROUND)
+                .view(imageBackground)
+                //.opacity(0.5)
+
+                .zIndex(-1)
+                .with(new IrremovableComponent())
+                .build();
+    }
+
+
+}//fin
